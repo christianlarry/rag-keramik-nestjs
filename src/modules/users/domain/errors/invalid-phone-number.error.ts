af23703f9def01faf2a8ee05dacbd22c7b71d059
christianlarry <christianlarry03@gmail.com>
2026-02-12 00:32:21 +0800

    Refactor users domain errors

--- src/modules/users/domain/errors/invalid-phone-number.error.ts
+++ src/modules/users/domain/errors/invalid-phone-number.error.ts
@@ -1,4 +1,4 @@
-import { DomainError } from "src/common/errors/domain.error";
+import { DomainError } from "src/core/domain/domain-error.base";
 import { UserErrorCode } from "./enums/user-error-code.enum";
 
 export class InvalidPhoneNumberError extends DomainError {
